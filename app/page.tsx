7cd8d74aa8c72045cb1f7fac7009d75e8f670901
seta <kafilisetayesh@gmail.com>
2025-12-12 13:03:29 +0330

    adding header and making a file for images

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Footer from "./pages/HomePage/Footer";
-import FAQ from "./pages/HomePage/F&Q";
+import Footer from "@/app/pages/HomePage/Footer";
+import FAQ from "@/app/pages/HomePage/F&Q";
+import Header from "@/app/pages/HomePage/Header";
 
 export default function Home() {
   return (
     <div className="">
+      <Header/>
       <FAQ />
       <Footer />
     </div>
